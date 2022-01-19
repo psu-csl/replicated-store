3f620f128558e433948c9e3bfb443ed0e3a491d8
Abutalib Aghayev <agayev@psu.edu>
2022-01-19 09:52:56 -0500

    switch to asio for networking

--- c++/replicant.cc
+++ c++/replicant.cc
@@ -5,64 +5,43 @@
 #include <unistd.h>
 #include <cassert>
 #include <cstring>
+#include <memory>
+#include <asio.hpp>
 
 #include "replicant.h"
 
 Replicant::Replicant()
     : consensus_(new Paxos(new MemStore())),
-      tp_(4),
-      client_fd_(-1),
-      done_(false) {
-
-  client_fd_ = socket(AF_INET, SOCK_STREAM, 0);
-  assert(client_fd_ != -1);
-
-  int optval = 1;
-  setsockopt(client_fd_, SOL_SOCKET, SO_REUSEADDR,
-	     (const void *)&optval , sizeof(int));
-
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(CLIENT_PORT);
-
-  int rc = bind(client_fd_, (struct sockaddr *) &addr, sizeof(addr));
-  assert(rc == 0);
-
-  rc = listen(client_fd_, 5);
-  assert(rc == 0);
+      tp_(8),
+      acceptor_(io_, tcp::endpoint(tcp::v4(), CLIENT_PORT)) {
+  acceptor_.listen(5);
 }
 
 Replicant::~Replicant() {
   tp_.join();
-  done_ = true;
 }
 
 void Replicant::Run() {
-  uint32_t n;
-  struct sockaddr_in addr;
-
-  while (!done_) {
-    int fd = accept(client_fd_, (struct sockaddr *) &addr, &n);
-    assert(fd != -1);
-    asio::post(tp_, [this, fd]() { HandleClient(fd); });
+  for (;;) {
+    socket_ptr cli(new tcp::socket(io_));
+    acceptor_.accept(*cli);
+    asio::post(tp_, [this, cli] { HandleClient(cli); });
   }
 }
 
-void Replicant::HandleClient(int fd) {
+void Replicant::HandleClient(socket_ptr cli) {
   for (;;) {
-    auto cmd = ReadCommand(fd);
+    auto cmd = ReadCommand(cli);
     if (cmd)
-      asio::post(tp_, [this, fd, cmd = std::move(*cmd)]() { HandleCommand(fd, cmd); });
+      asio::post(tp_, [this, cli, cmd = std::move(*cmd)]
+                      { HandleCommand(cli, std::move(cmd)); });
     else
       break;
   }
-  int rc = close(fd);
-  assert(rc == 0);
 }
 
-std::optional<Command> Replicant::ReadCommand(int fd) {
-  std::string line = ReadLine(fd);
+std::optional<Command> Replicant::ReadCommand(socket_ptr cli) {
+  std::string line = ReadLine(cli);
   if (line.empty())
     return std::nullopt;
   if (strncmp(line.c_str(), "get", 3) == 0)
@@ -75,40 +54,30 @@ std::optional<Command> Replicant::ReadCommand(int fd) {
   return Command {CommandType::kPut, line.substr(4, p-4), line.substr(p+1)};
 }
 
-std::string Replicant::ReadLine(int fd) {
+std::string Replicant::ReadLine(socket_ptr cli) {
   std::string line;
-  for (;;) {
-    char c;
-    ssize_t n = read(fd, &c, 1);
-    if (n == 1) {
-      if (c == '\n')
-        break;
-      line.push_back(c);
-    } else if (n == 0 || (n == -1 && errno != EINTR)) {
-      break;
-    }
-  }
+  asio::streambuf request;
+  asio::error_code ec;
+  asio::read_until(*cli.get(), request, '\n', ec);
+  if (ec.value() == 0)
+    std::getline(std::istream(&request), line);
   return line;
 }
 
-void Replicant::HandleCommand(int fd, Command cmd) {
+void Replicant::HandleCommand(socket_ptr cli, Command cmd) {
   bool is_get = cmd.type == CommandType::kGet;
   auto r = consensus_->AgreeAndExecute(std::move(cmd));
 
   static const std::string success = "success\n";
   static const std::string failure = "failure\n";
 
-  ssize_t n;
   if (r.ok) {
-    n = write(fd, success.c_str(), success.size());
-    assert(n == static_cast<ssize_t>(success.size()));
+    asio::write(*cli, asio::buffer(success));
     if (is_get) {
       r.value.push_back('\n');
-      n = write(fd, r.value.c_str(), r.value.size());
-      assert(n == static_cast<ssize_t>(r.value.size()));
+      asio::write(*cli, asio::buffer(r.value));
     }
   } else {
-    n = write(fd, failure.c_str(), failure.size());
-    assert(n == static_cast<ssize_t>(failure.size()));
+    asio::write(*cli, asio::buffer(failure));
   }
 }
--- c++/replicant.h
+++ c++/replicant.h
@@ -1,6 +1,7 @@
 #ifndef REPLICANT_H_
 #define REPLICANT_H_
 
+#include <memory>
 #include <optional>
 #include <asio.hpp>
 
@@ -10,21 +11,25 @@
 #define CLIENT_PORT 4444
 #define PEER_PORT   7777
 
+using asio::ip::tcp;
+
+typedef std::shared_ptr<tcp::socket> socket_ptr;
+
 class Replicant {
 public:
   Replicant();
   ~Replicant();
   void Run();
 private:
-  void HandleClient(int fd);
-  void HandleCommand(int fd, Command cmd);
-  std::optional<Command> ReadCommand(int fd);
-  std::string ReadLine(int fd);
+  void HandleClient(socket_ptr cli);
+  void HandleCommand(socket_ptr cli, Command cmd);
+  std::optional<Command> ReadCommand(socket_ptr cli);
+  std::string ReadLine(socket_ptr cli);
 
   std::unique_ptr<Consensus> consensus_;
   asio::thread_pool tp_;
-  int client_fd_;
-  bool done_;
+  asio::io_context io_;
+  tcp::acceptor acceptor_;
 };
 
 #endif
