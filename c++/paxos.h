ea6bb1639f062a1763199b8d294bdf9fc2ebec56
Abutalib Aghayev <agayev@psu.edu>
2022-01-25 10:34:12 -0500

    Forgot to add header.

--- c++/paxos.h
+++ c++/paxos.h
@@ -1,18 +1,25 @@
 #ifndef PAXOS_H_
 #define PAXOS_H_
 
+#include <condition_variable>
 #include <memory>
+#include <mutex>
 
 #include "consensus.h"
 #include "kvstore.h"
 
 class Paxos : public Consensus {
  public:
-  Paxos(KVStore* store) : store_(store) {}
+  Paxos(KVStore* store);
   ~Paxos() = default;
   Result AgreeAndExecute(Command command) override;
 
  private:
+  void HeartBeat(void);
+
+  bool leader_;
+  std::condition_variable cv_;
+  std::mutex mu_;
   std::unique_ptr<KVStore> store_;
 };
 
