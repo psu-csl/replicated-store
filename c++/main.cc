7637f01ba795f6bd9f964f666d7b09558da295d8
Abutalib Aghayev <agayev@psu.edu>
2022-01-27 10:35:48 -0500

    use log fatal instead of assert

--- c++/main.cc
+++ c++/main.cc
@@ -16,14 +16,15 @@ DEFINE_string(config, "config.json", "path to the configuration file");
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+  FLAGS_logtostderr = 1;
 
   std::ifstream f(FLAGS_config);
-  assert(f);
+  LOG_IF(FATAL, !f) << "failed to open " << FLAGS_config;
 
   json config;
   f >> config;
 
-  assert(FLAGS_me < config["peers"].size());
+  LOG_IF(FATAL, FLAGS_me >= config["peers"].size()) << FLAGS_me << config;
   config["me"] = FLAGS_me;
 
   Replicant replicant(config);
