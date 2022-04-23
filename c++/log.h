3fcd299c2d6b0f1abab5ce968f73defe9104ee66
Abutalib Aghayev <agayev@psu.edu>
2022-04-23 06:37:29 -0400

    c++: make Log not copyable and not movable

--- c++/log.h
+++ c++/log.h
@@ -1,17 +1,28 @@
 #ifndef LOG_H_
 #define LOG_H_
 
+#include <condition_variable>
 #include <cstdint>
+#include <mutex>
 #include <unordered_map>
 
 #include "instance.h"
 
 class Log {
+ public:
+  Log() = default;
+  Log(Log const& log) = delete;
+  Log& operator=(Log const& log) = delete;
+  Log(Log&& log) = delete;
+  Log& operator=(Log const& log) = delete;
+
  private:
   std::unordered_map<int64_t, Instance> log_;
-  int64_t last_inprogress_index_;
-  int64_t last_executed_index_;
-  int64_t global_last_executed_index_;
-
+  int64_t last_index_ = 0;
+  int64_t last_executed_index_ = 0;
+  int64_t global_last_executed_index_ = 0;
+  std::mutex mu_;
+  std::condition_variable cv_;
+};
 
 #endif
