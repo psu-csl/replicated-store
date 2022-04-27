9d1305ded0f292fa4ee7b4a2a7ecb12d4be5f9c2
Abutalib Aghayev <agayev@psu.edu>
2022-04-27 16:12:20 -0400

    c++, kvstore: add Execute method and capitalize method names

--- c++/kvstore.h
+++ c++/kvstore.h
@@ -1,12 +1,17 @@
 #ifndef KVSTORE_H_
 #define KVSTORE_H_
 
+#include <string>
+
+#include "command.h"
+
 class KVStore {
  public:
   virtual ~KVStore() = default;
-  virtual std::string* get(const std::string& key) = 0;
-  virtual bool put(const std::string& key, const std::string& value) = 0;
-  virtual bool del(const std::string& key) = 0;
+  virtual std::string* Get(const std::string& key) = 0;
+  virtual bool Put(const std::string& key, const std::string& value) = 0;
+  virtual bool Del(const std::string& key) = 0;
+  virtual Result Execute(const Command& cmd) = 0;
 };
 
 #endif
