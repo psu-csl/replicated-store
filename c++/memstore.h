c3b1b41bd3ed7b1dc1c585d14d4dc99b64d89e9c
Abutalib Aghayev <agayev@psu.edu>
2022-04-27 07:15:12 -0400

    c++, memstore: make memstore noncopyable, nonassignable

--- c++/memstore.h
+++ c++/memstore.h
@@ -1,15 +1,21 @@
 #include <map>
 #include <string>
 
+#include "command.h"
 #include "kvstore.h"
 
 class MemStore : public KVStore {
  public:
   MemStore() = default;
-  ~MemStore() = default;
-  std::string* get(const std::string& key) override;
-  bool put(const std::string& key, const std::string& value) override;
-  bool del(const std::string& key) override;
+  MemStore(MemStore const& store) = delete;
+  MemStore& operator=(MemStore const& store) = delete;
+  MemStore(MemStore&& store) = delete;
+  MemStore& operator=(MemStore&& store) = delete;
+
+  std::string* Get(const std::string& key) override;
+  bool Put(const std::string& key, const std::string& value) override;
+  bool Del(const std::string& key) override;
+  Result Execute(const Command& cmd) override;
 
  private:
   std::map<std::string, std::string> map_;
