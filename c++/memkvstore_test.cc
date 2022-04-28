47a009bb1ec41ab2503c757eb022c5667381a43b
Abutalib Aghayev <agayev@psu.edu>
2022-04-28 06:35:30 -0400

    c++, memkvstore: add unit test for get/put/del

--- c++/memkvstore_test.cc
+++ c++/memkvstore_test.cc
@@ -2,6 +2,33 @@
 
 #include "memkvstore.h"
 
-TEST(MemKVStoreTest, Constructor) {
+TEST(MemKVStoreTest, GetPutDel) {
   MemKVStore store;
+
+  std::string const key1 = "foo";
+  std::string const val1 = "bar";
+
+  std::string const key2 = "baz";
+  std::string const val2 = "quux";
+
+  EXPECT_EQ(nullptr, store.Get(key1));
+
+  EXPECT_FALSE(store.Del(key1));
+
+  EXPECT_TRUE(store.Put(key1, val1));
+  EXPECT_EQ(val1, *store.Get(key1));
+  EXPECT_TRUE(store.Put(key2, val2));
+  EXPECT_EQ(val2, *store.Get(key2));
+
+  EXPECT_TRUE(store.Put(key1, val2));
+  EXPECT_EQ(val2, *store.Get(key1));
+  EXPECT_EQ(val2, *store.Get(key2));
+
+  EXPECT_TRUE(store.Del(key1));
+  EXPECT_EQ(nullptr, store.Get(key1));
+  EXPECT_EQ(val2, *store.Get(key2));
+
+  EXPECT_TRUE(store.Del(key2));
+  EXPECT_EQ(nullptr, store.Get(key1));
+  EXPECT_EQ(nullptr, store.Get(key2));
 }
