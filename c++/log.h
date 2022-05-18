cdc0fde44d5e00a8004c89c7a128dcb917fb5798
Abutalib Aghayev <agayev@psu.edu>
2022-05-18 16:22:13 -0400

    c++: log: use Instance's member function to check the state.

--- c++/log.h
+++ c++/log.h
@@ -40,7 +40,7 @@ class Log {
 
   bool IsExecutable(void) const {
     auto it = log_.find(last_executed_ + 1);
-    return it != log_.end() && it->second.state_ == InstanceState::kCommitted;
+    return it != log_.end() && it->second.IsCommitted();
   }
 
   Instance const* operator[](std::size_t i) const;
