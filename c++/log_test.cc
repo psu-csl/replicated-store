3c6f135d78b32ce32b639e873cea955f0cff84d1
Abutalib Aghayev <agayev@psu.edu>
2022-05-25 08:28:00 -0400

    c++: log_test: use text fixture to reduce boilerplate code
    also added a new function, MakeInstance, and several overloads to further reduce
    boilerplate code.

--- c++/log_test.cc
+++ c++/log_test.cc
@@ -2,283 +2,195 @@
 #include <atomic>
 #include <thread>
 
+#include "instance.h"
 #include "log.h"
 #include "memkvstore.h"
 
-TEST(LogTest, Constructor) {
-  Log log;
+class LogTest : public testing::Test {
+ protected:
+  Instance MakeInstance(int64_t ballot) {
+    return Instance{ballot, log_.AdvanceLastIndex(), 0,
+                    InstanceState::kInProgress, Command{}};
+  }
+  Instance MakeInstance(int64_t ballot, int64_t index) {
+    return Instance{ballot, index, 0, InstanceState::kInProgress, Command{}};
+  }
+  Instance MakeInstance(int64_t ballot, InstanceState state) {
+    return Instance{ballot, log_.AdvanceLastIndex(), 0, state, Command{}};
+  }
+  Instance MakeInstance(int64_t ballot, int64_t index, CommandType type) {
+    return Instance{ballot, index, 0, InstanceState::kInProgress,
+                    Command{type, "", ""}};
+  }
+  Instance MakeInstance(int64_t ballot,
+                        int64_t index,
+                        InstanceState state,
+                        CommandType type) {
+    return Instance{ballot, index, 0, state, Command{type, "", ""}};
+  }
 
-  EXPECT_EQ(log.LastExecuted(), 0);
-  EXPECT_EQ(log.GlobalLastExecuted(), 0);
-  EXPECT_FALSE(log.IsExecutable());
-  EXPECT_EQ(nullptr, log[0]);
-  EXPECT_EQ(nullptr, log[-1]);
-  EXPECT_EQ(nullptr, log[3]);
+  Log log_;
+  MemKVStore store_;
+};
+
+using LogDeathTest = LogTest;
+
+TEST_F(LogTest, Constructor) {
+  EXPECT_EQ(log_.LastExecuted(), 0);
+  EXPECT_EQ(log_.GlobalLastExecuted(), 0);
+  EXPECT_FALSE(log_.IsExecutable());
+  EXPECT_EQ(nullptr, log_[0]);
+  EXPECT_EQ(nullptr, log_[-1]);
+  EXPECT_EQ(nullptr, log_[3]);
 }
 
-TEST(LogTest, Append) {
-  // append two instances at indexes 1 and 2 to the log and ensure they are in
-  // the log.
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-    EXPECT_EQ(index, log[index]->index_);
-
-    index = log.AdvanceLastIndex();
-    Instance i2{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i2));
-    EXPECT_EQ(index, log[index]->index_);
-  }
-  // when appending an instance in executed state, its state should be updated
-  // to committed.
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kExecuted, cmd};
-    log.Append(std::move(i1));
-    EXPECT_TRUE(log[index]->IsCommitted());
-  }
-  // when append an instance at an index higher than last_index_, last_index_
-  // should be updated.
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index = 42;
-    Instance i1{0, index, 0, InstanceState::kExecuted, cmd};
-    log.Append(std::move(i1));
-    EXPECT_EQ(index, log[index]->index_);
-    EXPECT_EQ(index + 1, log.AdvanceLastIndex());
-  }
-  // appending an instance at an index containing an instance with a lower
-  // ballot number should replace the instance in the log.
-  {
-    Log log;
-    Command cmd1{CommandType::kPut, "", ""};
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd1};
-    log.Append(std::move(i1));
-    EXPECT_EQ(index, log[index]->index_);
-    EXPECT_EQ(CommandType::kPut, log[index]->command_.type_);
-
-    Command cmd2{CommandType::kDel, "", ""};
-    Instance i2{1, index, 0, InstanceState::kInProgress, cmd2};
-    log.Append(std::move(i2));
-    EXPECT_EQ(CommandType::kDel, log[index]->command_.type_);
-  }
-  // appending an instance at an index containing an instance with a lower
-  // ballot number should have no effect.
-  {
-    Log log;
-    Command cmd1{CommandType::kPut, "", ""};
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{2, index, 0, InstanceState::kInProgress, cmd1};
-    log.Append(std::move(i1));
-    EXPECT_EQ(index, log[index]->index_);
-    EXPECT_EQ(CommandType::kPut, log[index]->command_.type_);
-
-    Command cmd2{CommandType::kDel, "", ""};
-    Instance i2{1, index, 0, InstanceState::kInProgress, cmd2};
-    log.Append(std::move(i2));
-    EXPECT_EQ(CommandType::kPut, log[index]->command_.type_);
-  }
-  // filling the gaps in the log (i.e. inserting entries at indexes lower than
-  // the current value of last_index_) should not affect last_index_
-  {
-    Log log;
-    Command cmd;
-    int64_t index = 42;
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-
-    Instance i2{0, index - 10, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i2));
-
-    EXPECT_EQ(index + 1, log.AdvanceLastIndex());
-  }
+TEST_F(LogTest, Append) {
+  log_.Append(MakeInstance(0));
+  log_.Append(MakeInstance(0));
+  EXPECT_EQ(1, log_[1]->index_);
+  EXPECT_EQ(2, log_[2]->index_);
 }
 
-TEST(LogDeathTest, Append) {
-  // ensure that the assertion fires for case (3) from the design doc.
-  {
-    Log log;
-    Command cmd1;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kCommitted, cmd1};
-    log.Append(std::move(i1));
-
-    Command cmd2{CommandType::kPut, "", ""};
-    Instance i2{0, index, 0, InstanceState::kInProgress, cmd2};
-    EXPECT_DEATH(log.Append(std::move(i2)), "case 3");
-  }
-  // same as above, except when the instance already in the log is in executed
-  // state.
-  {
-    Log log;
-    Command cmd1;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kExecuted, cmd1};
-    log.Append(std::move(i1));
-
-    Command cmd2{CommandType::kPut, "", ""};
-    Instance i2{0, index, 0, InstanceState::kInProgress, cmd2};
-    EXPECT_DEATH(log.Append(std::move(i2)), "case 3");
-  }
-  // ensure that the assertion fires for case (4) from the design doc.
-  {
-    Log log;
-    Command cmd1{CommandType::kPut, "", ""};
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd1};
-    log.Append(std::move(i1));
-
-    Command cmd2{CommandType::kDel, "", ""};
-    Instance i2{0, index, 0, InstanceState::kInProgress, cmd2};
-    EXPECT_DEATH(log.Append(std::move(i2)), "case 4");
-  }
+TEST_F(LogTest, AppendExecuted) {
+  log_.Append(MakeInstance(0, InstanceState::kExecuted));
+  EXPECT_TRUE(log_[1]->IsCommitted());
 }
 
-TEST(LogTest, Commit) {
-  // common case: an instance gets appended and committing it changes its state
-  // to committed.
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-    EXPECT_TRUE(log[index]->IsInProgress());
-    log.Commit(index);
-    EXPECT_TRUE(log[index]->IsCommitted());
-  }
+TEST_F(LogTest, AppendWithGap) {
+  auto index = 42;
+  log_.Append(MakeInstance(0, index));
+  EXPECT_EQ(index, log_[index]->index_);
+  EXPECT_EQ(index + 1, log_.AdvanceLastIndex());
 }
 
-TEST(LogTest, AppendCommit) {
-  // if we append and instance and commit it, the log should become executable
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-    EXPECT_FALSE(log.IsExecutable());
-
-    log.Commit(index);
-    EXPECT_TRUE(log.IsExecutable());
-  }
-  // if we append two instances in sequence and commit the second one, the log
-  // should not become executable; committing the first one should make the log
-  // executable again.
-  {
-    Log log;
-    Command cmd;
-
-    int64_t index1 = log.AdvanceLastIndex();
-    Instance i1{0, index1, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-
-    int64_t index2 = log.AdvanceLastIndex();
-    Instance i2{0, index2, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i2));
-
-    log.Commit(index2);
-    EXPECT_FALSE(log.IsExecutable());
-    log.Commit(index1);
-    EXPECT_TRUE(log.IsExecutable());
-  }
-  // if commit is called first on an index where there is no instance yet, it
-  // should still eventually succeed when append is called and the instance is
-  // put at index.
-  {
-    Log log;
-    Command cmd;
-    int64_t index = log.AdvanceLastIndex();
-    std::thread commit([&log, index] { log.Commit(index); });
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    std::this_thread::yield();
-    log.Append(std::move(i1));
-    commit.join();
-    EXPECT_TRUE(log[index]->IsCommitted());
-  }
+TEST_F(LogTest, AppendFillGaps) {
+  auto index = 42;
+  log_.Append(MakeInstance(0, index));
+  log_.Append(MakeInstance(0, index - 10));
+  EXPECT_EQ(index + 1, log_.AdvanceLastIndex());
 }
 
-TEST(LogTest, AppendCommitExecute) {
-  // simplest case: append an entry, commit it, and execute it.
-  {
-    Log log;
-    MemKVStore store;
-    Command cmd;
-    std::atomic<bool> done = false;
-
-    std::thread execute([&done, &log, &store] {
-      while (!done)
-        log.Execute(&store);
-    });
-
-    int64_t index = log.AdvanceLastIndex();
-    Instance i1{0, index, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-
-    done = true;
-    log.Commit(index);
-    execute.join();
-
-    EXPECT_TRUE(log[index]->IsExecuted());
-    EXPECT_EQ(index, log.LastExecuted());
-  }
-  // append entries out of order, commit them in reverse order, and ensure that
-  // all are executed.
-  {
-    Log log;
-    MemKVStore store;
-    Command cmd;
-
-    std::thread execute([&log, &store] {
-      log.Execute(&store);
-      log.Execute(&store);
-      log.Execute(&store);
-    });
-
-    int64_t index1 = log.AdvanceLastIndex();
-    Instance i1{0, index1, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i1));
-
-    int64_t index2 = log.AdvanceLastIndex();
-    Instance i2{0, index2, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i2));
-
-    int64_t index3 = log.AdvanceLastIndex();
-    Instance i3{0, index3, 0, InstanceState::kInProgress, cmd};
-    log.Append(std::move(i3));
-
-    log.Commit(index3);
-    log.Commit(index2);
-    log.Commit(index1);
-    execute.join();
-
-    EXPECT_TRUE(log[index1]->IsExecuted());
-    EXPECT_TRUE(log[index2]->IsExecuted());
-    EXPECT_TRUE(log[index3]->IsExecuted());
-    EXPECT_EQ(index3, log.LastExecuted());
-  }
+TEST_F(LogTest, AppendHighBallotOverride) {
+  auto index = 1, lo_ballot = 0, hi_ballot = 1;
+  log_.Append(MakeInstance(lo_ballot, index, CommandType::kPut));
+  log_.Append(MakeInstance(hi_ballot, index, CommandType::kDel));
+  EXPECT_EQ(CommandType::kDel, log_[index]->command_.type_);
 }
 
-TEST(LogDeathTest, Commit) {
-  Log log;
+TEST_F(LogTest, AppendLowBallotNoEffect) {
+  auto index = 1, lo_ballot = 0, hi_ballot = 1;
+  log_.Append(MakeInstance(hi_ballot, index, CommandType::kPut));
+  log_.Append(MakeInstance(lo_ballot, index, CommandType::kDel));
+  EXPECT_EQ(CommandType::kPut, log_[index]->command_.type_);
+}
 
-  EXPECT_DEATH(log.Commit(0), "invalid index");
-  EXPECT_DEATH(log.Commit(-1), "invalid index");
+TEST_F(LogDeathTest, AppendCase3Committed) {
+  auto index = 1;
+  auto inst1 =
+      MakeInstance(0, index, InstanceState::kCommitted, CommandType::kPut);
+  auto inst2 =
+      MakeInstance(0, index, InstanceState::kInProgress, CommandType::kDel);
+  log_.Append(std::move(inst1));
+  EXPECT_DEATH(log_.Append(std::move(inst2)), "case 3");
+}
+
+TEST_F(LogDeathTest, AppendCase3Executed) {
+  auto index = 1;
+  auto inst1 =
+      MakeInstance(0, index, InstanceState::kExecuted, CommandType::kPut);
+  auto inst2 =
+      MakeInstance(0, index, InstanceState::kInProgress, CommandType::kDel);
+  log_.Append(std::move(inst1));
+  EXPECT_DEATH(log_.Append(std::move(inst2)), "case 3");
+}
+
+TEST_F(LogDeathTest, AppendCase4) {
+  auto index = 1;
+  auto inst1 =
+      MakeInstance(0, index, InstanceState::kInProgress, CommandType::kPut);
+  auto inst2 =
+      MakeInstance(0, index, InstanceState::kInProgress, CommandType::kDel);
+  log_.Append(std::move(inst1));
+  EXPECT_DEATH(log_.Append(std::move(inst2)), "case 4");
+}
+
+TEST_F(LogTest, Commit) {
+  auto index1 = 1;
+  log_.Append(MakeInstance(0, index1));
+  auto index2 = 2;
+  log_.Append(MakeInstance(0, index2));
+
+  EXPECT_TRUE(log_[index1]->IsInProgress());
+  EXPECT_TRUE(log_[index2]->IsInProgress());
+  EXPECT_FALSE(log_.IsExecutable());
+
+  log_.Commit(index2);
+
+  EXPECT_TRUE(log_[index1]->IsInProgress());
+  EXPECT_TRUE(log_[index2]->IsCommitted());
+  EXPECT_FALSE(log_.IsExecutable());
+
+  log_.Commit(index1);
+
+  EXPECT_TRUE(log_[index1]->IsCommitted());
+  EXPECT_TRUE(log_[index2]->IsCommitted());
+  EXPECT_TRUE(log_.IsExecutable());
+}
+
+TEST_F(LogDeathTest, Commit) {
+  EXPECT_DEATH(log_.Commit(0), "invalid index");
+  EXPECT_DEATH(log_.Commit(-1), "invalid index");
+}
+
+TEST_F(LogTest, CommitBeforeAppend) {
+  auto index = 1;
+  std::thread commit_thread([this, index] { log_.Commit(index); });
+  std::this_thread::yield();
+  log_.Append(MakeInstance(0));
+  commit_thread.join();
+  EXPECT_TRUE(log_[index]->IsCommitted());
+}
+
+TEST_F(LogTest, AppendCommitExecute) {
+  auto index = 1;
+  std::atomic<bool> done = false;
+
+  std::thread execute_thread([this, &done] {
+    while (!done)
+      log_.Execute(&store_);
+  });
+
+  log_.Append(MakeInstance(0, index));
+  done = true;
+  log_.Commit(index);
+  execute_thread.join();
+
+  EXPECT_TRUE(log_[index]->IsExecuted());
+  EXPECT_EQ(index, log_.LastExecuted());
+}
+
+TEST_F(LogTest, AppendCommitExecuteOutOfOrder) {
+  std::thread execute_thread([this] {
+    log_.Execute(&store_);
+    log_.Execute(&store_);
+    log_.Execute(&store_);
+  });
+
+  auto index1 = 1;
+  log_.Append(MakeInstance(0, index1));
+  auto index2 = 2;
+  log_.Append(MakeInstance(0, index2));
+  auto index3 = 3;
+  log_.Append(MakeInstance(0, index3));
+
+  log_.Commit(index3);
+  log_.Commit(index2);
+  log_.Commit(index1);
+
+  execute_thread.join();
+
+  EXPECT_TRUE(log_[index1]->IsExecuted());
+  EXPECT_TRUE(log_[index2]->IsExecuted());
+  EXPECT_TRUE(log_[index3]->IsExecuted());
+  EXPECT_EQ(index3, log_.LastExecuted());
 }
