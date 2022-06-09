6c081af7dd74746ab166acdb7d19dbd450fdac68
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2022-06-09 20:39:23 +0300

    java: configuration: add port to Configuration

--- java/replicator/src/main/java/paxos/Configuration.java
+++ java/replicator/src/main/java/paxos/Configuration.java
@@ -3,6 +3,7 @@ package paxos;
 public class Configuration {
 
   private long id;
+  private int port;
 
   public long getId() {
     return id;
@@ -10,5 +11,13 @@ public class Configuration {
 
   public void setId(long id) {
     this.id = id;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
   }
 }
