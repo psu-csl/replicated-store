bfb077988be6cfa8154512aad95bb0e0f1d927e7
Zhiying Liang <zvl5490@psu.edu>
2022-06-10 02:23:02 -0400

    go: multipaxos: Test heartbeat handler with different ballot
    It tests the cases when the ballot of the request is greater, equal,
    or less than its own ballot. As we do not implement other parts, this
    unit test doesn't involve the status of log_. It only checks whether
    ballot, lastExecuted are expected.

--- go/consensus/multipaxos/multipaxos_test.go
+++ go/consensus/multipaxos/multipaxos_test.go
@@ -1,7 +1,9 @@
 package multipaxos
 
 import (
+	"context"
 	"github.com/psu-csl/replicated-store/go/config"
+	pb "github.com/psu-csl/replicated-store/go/consensus/multipaxos/comm"
 	"github.com/psu-csl/replicated-store/go/log"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -56,4 +58,46 @@ func TestNextBallot(t *testing.T) {
 		assert.False(t, mp.IsSomeoneElseLeader())
 		assert.Equal(t, id, mp.Leader())
 	}
+}
+
+func TestHeartbeatHandlerBallot(t *testing.T) {
+	setup()
+
+	const (
+		staleBallot  = MaxNumPeers + 1
+		leaderId = MaxNumPeers - 1
+	)
+	leader := NewMultipaxos(config.Config{Id: leaderId}, log.NewLog())
+	ballot := leader.NextBallot()
+
+	// Higher Ballot number
+	request := pb.HeartbeatRequest{
+		Ballot:             ballot,
+		LastExecuted:       1,
+		GlobalLastExecuted: 0,
+	}
+	_, err := multiPaxos.HeartbeatHandler(context.Background(), &request)
+	ts := multiPaxos.LastHeartbeat()
+	// maybe useful for grpc testing
+	assert.Nil(t, err)
+	assert.Equal(t, ballot, multiPaxos.Ballot())
+	assert.True(t, multiPaxos.IsSomeoneElseLeader())
+	assert.Equal(t, leaderId, multiPaxos.Leader())
+
+	// Stale heartbeat with lower ballot number
+	request2 := pb.HeartbeatRequest{
+		Ballot:             staleBallot,
+		LastExecuted:       1,
+		GlobalLastExecuted: 0,
+	}
+	_, err = multiPaxos.HeartbeatHandler(context.Background(), &request2)
+	assert.Nil(t, err)
+	assert.Equal(t, ballot, multiPaxos.Ballot())
+	assert.Equal(t, ts, multiPaxos.LastHeartbeat())
+
+	// The repeated heartbeat from the same leader
+	_, err = multiPaxos.HeartbeatHandler(context.Background(), &request)
+	assert.Nil(t, err)
+	assert.Equal(t, ballot, multiPaxos.Ballot())
+	assert.True(t, multiPaxos.LastHeartbeat().After(ts))
 }
