10c883450bc8995c3f9ca50f8135f03858b9ceb1
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2022-06-15 22:37:46 +0300

    java: multipaxos_test: implement a basic test
    Test grpc server, channel and request sending.

--- java/replicator/src/test/java/paxos/MultiPaxosTest.java
+++ java/replicator/src/test/java/paxos/MultiPaxosTest.java
@@ -4,7 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import command.Command;
+import command.Command.CommandType;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import kvstore.MemKVStore;
+import log.Instance;
+import log.Instance.InstanceState;
 import log.Log;
+import multipaxosrpc.HeartbeatRequest;
+import multipaxosrpc.HeartbeatResponse;
+import multipaxosrpc.MultiPaxosRPCGrpc;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +27,30 @@ class MultiPaxosTest {
   protected MultiPaxos multiPaxos;
   protected Configuration config;
 
+  protected ExecutorService server = Executors.newSingleThreadExecutor();
+
+
+  public Instance MakeInstance(long ballot, long index, InstanceState state, CommandType type) {
+    return new Instance(ballot, index, 0, state, new Command(type, "", ""));
+  }
+
   @BeforeEach
   void setUp() {
     config = new Configuration();
     config.setId(0);
+    config.setPort(8080);
     log = new Log();
     multiPaxos = new MultiPaxos(log, config);
+    server.submit(() -> {
+      multiPaxos.startServer();
+      multiPaxos.blockUntilShutDown();
+    });
+
+  }
+
+  @AfterEach
+  void tearDown() {
+    multiPaxos.stopServer();
   }
 
   @Test
@@ -46,5 +77,31 @@ class MultiPaxosTest {
       assertFalse(mp.isSomeoneElseLeader());
       assertEquals(id, mp.leader());
     }
+  }
+
+  @Test
+  void acceptHandler() {
+    MemKVStore store = new MemKVStore();
+
+    log.append(MakeInstance(17, 1, InstanceState.kExecuted, CommandType.kPut));
+    log.append(MakeInstance(17, 2, InstanceState.kInProgress, CommandType.kGet));
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(() -> {
+      log.execute(store);
+      log.execute(store);
+    });
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext()
+        .build();
+    var blockingStub = MultiPaxosRPCGrpc.newBlockingStub(channel);
+
+    HeartbeatRequest request = HeartbeatRequest.newBuilder().setBallot(18).setLastExecuted(2)
+        .setGlobalLastExecuted(1)
+        .build();
+
+    HeartbeatResponse response = blockingStub.heartbeat(request);
+    // System.out.println("Response is " + response.getLastExecuted());
+    assertEquals(log.getLastExecuted(), response.getLastExecuted());
+
+
   }
 }
