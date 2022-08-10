337029d05e77c8bcdad1eb3e2396b11f006f098f
Zhiying Liang <zvl5490@psu.edu>
2022-08-10 00:59:20 -0400

    go: multipaxos_test: put peers, logs, configs, and stores in array
    also add helper function to send stub request

--- go/consensus/multipaxos/multipaxos_test.go
+++ go/consensus/multipaxos/multipaxos_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/psu-csl/replicated-store/go/config"
 	pb "github.com/psu-csl/replicated-store/go/consensus/multipaxos/comm"
 	"github.com/psu-csl/replicated-store/go/log"
+	"github.com/psu-csl/replicated-store/go/store"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,125 +16,123 @@ import (
 const N = 3
 
 var (
-	log0  *log.Log
-	log1  *log.Log
-	log2  *log.Log
-	peer0 *Multipaxos
-	peer1 *Multipaxos
-	peer2 *Multipaxos
+	configs []*config.Config
+	logs    []*log.Log
+	peers   []*Multipaxos
+	stores  []*store.MemKVStore
 )
 
 func setupPeers() {
-	log0 = log.NewLog()
-	log1 = log.NewLog()
-	log2 = log.NewLog()
-	peer0 = NewMultipaxos(config.DefaultConfig(0, N), log0)
-	peer1 = NewMultipaxos(config.DefaultConfig(1, N), log1)
-	peer2 = NewMultipaxos(config.DefaultConfig(2, N), log2)
+	logs[0] = log.NewLog()
+	logs[1] = log.NewLog()
+	logs[2] = log.NewLog()
+	peers[0] = NewMultipaxos(config.DefaultConfig(0, N), logs[0])
+	peers[1] = NewMultipaxos(config.DefaultConfig(1, N), logs[1])
+	peers[2] = NewMultipaxos(config.DefaultConfig(2, N), logs[2])
 }
 
 func setup() {
 	setupPeers()
-	peer0.Start()
-	peer1.Start()
-	peer2.Start()
+	peers[0].Start()
+	peers[1].Start()
+	peers[2].Start()
 }
 
 func setupOnePeer() {
-	log0 = log.NewLog()
-	peer0 = NewMultipaxos(config.DefaultConfig(0, 1), log0)
-	peer0.StartServer()
+	logs[0] = log.NewLog()
+	peers[0] = NewMultipaxos(config.DefaultConfig(0, 1), logs[0])
+	peers[0].StartServer()
 }
 
 func tearDown() {
-	peer0.Stop()
-	peer1.Stop()
-	peer2.Stop()
+	peers[0].Stop()
+	peers[1].Stop()
+	peers[2].Stop()
 }
 
 func TestNewMultipaxos(t *testing.T) {
-	log0 = log.NewLog()
-	peer0 = NewMultipaxos(config.DefaultConfig(0, N), log0)
+	logs[0] = log.NewLog()
+	peers[0] = NewMultipaxos(config.DefaultConfig(0, N), logs[0])
 
-	assert.Equal(t, MaxNumPeers, LeaderByPeer(peer0))
-	assert.False(t, IsLeaderByPeer(peer0))
-	assert.False(t, IsSomeoneElseLeaderByPeer(peer0))
+	assert.Equal(t, MaxNumPeers, LeaderByPeer(peers[0]))
+	assert.False(t, IsLeaderByPeer(peers[0]))
+	assert.False(t, IsSomeoneElseLeaderByPeer(peers[0]))
 }
 
 func TestNextBallot(t *testing.T) {
-	log2 = log.NewLog()
-	peer2 = NewMultipaxos(config.DefaultConfig(2, N), log0)
-	id := peer2.Id()
+	logs[2] = log.NewLog()
+	peers[2] = NewMultipaxos(config.DefaultConfig(2, N), logs[0])
+	id := peers[2].Id()
 	ballot := id
 
 	ballot += RoundIncrement
-	assert.Equal(t, ballot, peer2.NextBallot())
+	assert.Equal(t, ballot, peers[2].NextBallot())
 	ballot += RoundIncrement
-	assert.Equal(t, ballot, peer2.NextBallot())
+	assert.Equal(t, ballot, peers[2].NextBallot())
 
-	assert.True(t, IsLeaderByPeer(peer2))
-	assert.False(t, IsSomeoneElseLeaderByPeer(peer2))
-	assert.Equal(t, id, LeaderByPeer(peer2))
+	assert.True(t, IsLeaderByPeer(peers[2]))
+	assert.False(t, IsSomeoneElseLeaderByPeer(peers[2]))
+	assert.Equal(t, id, LeaderByPeer(peers[2]))
 }
 
 func TestHeartbeatIgnoreStaleRPC(t *testing.T) {
 	setupPeers()
-	peer0.StartServer()
+	peers[0].StartServer()
 	stub := createStub()
 
-	peer0.NextBallot()
-	peer0.NextBallot()
+	peers[0].NextBallot()
+	peers[0].NextBallot()
 
 	ctx := context.Background()
 	request := pb.HeartbeatRequest{
-		Ballot: peer1.NextBallot(),
+		Ballot: peers[1].NextBallot(),
 	}
 	_, err := stub.Heartbeat(ctx, &request)
 
 	assert.Nil(t, err)
-	assert.True(t, IsLeaderByPeer(peer0))
+	assert.True(t, IsLeaderByPeer(peers[0]))
 
-	peer0.Stop()
+	peers[0].Stop()
 }
 
 func TestHeartbeatChangesLeaderToFollower(t *testing.T) {
 	setupPeers()
-	peer0.StartServer()
+	peers[0].StartServer()
 	stub := createStub()
 
-	peer0.NextBallot()
+	peers[0].NextBallot()
 
 	ctx := context.Background()
 	request := pb.HeartbeatRequest{
-		Ballot: peer1.NextBallot(),
+		Ballot: peers[1].NextBallot(),
 	}
 	_, err := stub.Heartbeat(ctx, &request)
 
 	assert.Nil(t, err)
-	assert.False(t, IsLeaderByPeer(peer0))
-	assert.EqualValues(t, 1, LeaderByPeer(peer0))
+	assert.False(t, IsLeaderByPeer(peers[0]))
+	assert.EqualValues(t, 1, LeaderByPeer(peers[0]))
 
-	peer0.Stop()
+	peers[0].Stop()
 }
 
 func TestNextBallotAfterHeartbeat(t *testing.T) {
 	setupPeers()
-	peer0.StartServer()
+	peers[0].StartServer()
 	stub := createStub()
-	ballot := peer0.Id()
+	ballot := peers[0].Id()
 
 	ctx := context.Background()
 	request := pb.HeartbeatRequest{
-		Ballot: peer1.NextBallot(),
+		Ballot: peers[1].NextBallot(),
 	}
 	stub.Heartbeat(ctx, &request)
-	assert.EqualValues(t, 1, LeaderByPeer(peer0))
+	assert.EqualValues(t, 1, LeaderByPeer(peers[0]))
 
 	ballot += RoundIncrement
 	ballot += RoundIncrement
-	assert.EqualValues(t, ballot, peer0.NextBallot())
+	assert.EqualValues(t, ballot, peers[0].NextBallot())
 
-	peer0.Stop()
+	peers[0].Stop()
 }
 
 func TestOneLeaderElected(t *testing.T) {
@@ -147,7 +146,7 @@ func TestOneLeaderElected(t *testing.T) {
 func TestReplicateRetry(t *testing.T) {
 	setupPeers()
 
-	result := peer0.Replicate(&pb.Command{Type: pb.CommandType_GET}, 0)
+	result := peers[0].Replicate(&pb.Command{Type: pb.CommandType_GET}, 0)
 	assert.Equal(t, Retry, result.Type)
 	assert.Equal(t, NoLeader, result.Leader)
 }
@@ -155,19 +154,19 @@ func TestReplicateRetry(t *testing.T) {
 func TestReplicateWithLeader(t *testing.T) {
 	setupPeers()
 
-	peer1.StartServer()
-	peer2.StartServer()
-	peer0.Start()
+	peers[1].StartServer()
+	peers[2].StartServer()
+	peers[0].Start()
 	time.Sleep(1000 * time.Millisecond)
-	result := peer0.Replicate(&pb.Command{Type: pb.CommandType_PUT}, 0)
+	result := peers[0].Replicate(&pb.Command{Type: pb.CommandType_PUT}, 0)
 	assert.Equal(t, Ok, result.Type)
 	assert.Equal(t, int64(0), result.Leader)
 
-	result = peer1.Replicate(&pb.Command{Type: pb.CommandType_DEL}, 0)
+	result = peers[1].Replicate(&pb.Command{Type: pb.CommandType_DEL}, 0)
 	assert.Equal(t, SomeElseLeader, result.Type)
 	assert.Equal(t, int64(0), result.Leader)
 
-	peer0.Stop()
+	peers[0].Stop()
 }
 
 func createStub() pb.MultiPaxosRPCClient {
@@ -181,17 +180,17 @@ func createStub() pb.MultiPaxosRPCClient {
 }
 
 func oneLeader() bool {
-	if IsLeaderByPeer(peer0) {
-		return !IsLeaderByPeer(peer1) && !IsLeaderByPeer(peer2) &&
-			LeaderByPeer(peer1) == 0 && LeaderByPeer(peer2) == 0
+	if IsLeaderByPeer(peers[0]) {
+		return !IsLeaderByPeer(peers[1]) && !IsLeaderByPeer(peers[2]) &&
+			LeaderByPeer(peers[1]) == 0 && LeaderByPeer(peers[2]) == 0
 	}
-	if IsLeaderByPeer(peer1) {
-		return !IsLeaderByPeer(peer0) && !IsLeaderByPeer(peer2) &&
-			LeaderByPeer(peer0) == 1 && LeaderByPeer(peer2) == 1
+	if IsLeaderByPeer(peers[1]) {
+		return !IsLeaderByPeer(peers[0]) && !IsLeaderByPeer(peers[2]) &&
+			LeaderByPeer(peers[0]) == 1 && LeaderByPeer(peers[2]) == 1
 	}
-	if IsLeaderByPeer(peer2) {
-		return !IsLeaderByPeer(peer0) && !IsLeaderByPeer(peer1) &&
-			LeaderByPeer(peer0) == 2 && LeaderByPeer(peer1) == 2
+	if IsLeaderByPeer(peers[2]) {
+		return !IsLeaderByPeer(peers[0]) && !IsLeaderByPeer(peers[1]) &&
+			LeaderByPeer(peers[0]) == 2 && LeaderByPeer(peers[1]) == 2
 	}
 	return false
 }
@@ -208,4 +207,42 @@ func IsLeaderByPeer(peer *Multipaxos) bool {
 
 func IsSomeoneElseLeaderByPeer(peer *Multipaxos) bool {
 	return !IsLeaderByPeer(peer) && LeaderByPeer(peer) < MaxNumPeers
+}
+
+func sendHeartbeat(stub pb.MultiPaxosRPCClient, ballot int64,
+	lastExecuted int64, globalLastExecuted int64) *pb.HeartbeatResponse {
+	ctx := context.Background()
+	request := pb.HeartbeatRequest{
+		Ballot:             ballot,
+		LastExecuted:       lastExecuted,
+		GlobalLastExecuted: globalLastExecuted,
+	}
+	response, err := stub.Heartbeat(ctx, &request)
+	if err != nil {
+		return nil
+	}
+	return response
+}
+
+func sendPrepare(stub pb.MultiPaxosRPCClient, ballot int64) *pb.PrepareResponse {
+	ctx := context.Background()
+	request := pb.PrepareRequest{Ballot: ballot}
+	response, err := stub.Prepare(ctx, &request)
+	if err != nil {
+		return nil
+	}
+	return response
+}
+
+func sendAccept(stub pb.MultiPaxosRPCClient,
+	inst *pb.Instance) *pb.AcceptResponse {
+	ctx := context.Background()
+	request := pb.AcceptRequest{
+		Instance: inst,
+	}
+	response, err := stub.Accept(ctx, &request)
+	if err != nil {
+		return nil
+	}
+	return response
 }
