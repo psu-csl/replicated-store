85aa7785a684060e24156e07333ed46d4ad22e07
Zhiying Liang <zvl5490@psu.edu>
2022-08-11 23:40:24 -0400

    go: test_util: simplify code and add make_instance_with_state

--- go/consensus/multipaxos/util/test_util.go
+++ go/consensus/multipaxos/util/test_util.go
@@ -7,15 +7,25 @@ func MakeInstance(ballot int64, index int64) *comm.Instance {
 		Index: index, State: comm.InstanceState_INPROGRESS, ClientId: 0}
 }
 
+func MakeInstanceWithState(ballot int64, index int64,
+	state comm.InstanceState) *comm.Instance {
+	instance := MakeInstance(ballot, index)
+	instance.State = state
+	return instance
+}
+
 func MakeInstanceWithType(ballot int64, index int64,
 	cmdType comm.CommandType) *comm.Instance {
-	return &comm.Instance{Ballot: ballot, Command: &comm.Command{Type: cmdType},
-		Index: index, State: comm.InstanceState_INPROGRESS, ClientId: 0}
+	instance := MakeInstance(ballot, index)
+	instance.Command.Type = cmdType
+	return instance
 }
 
 func MakeInstanceWithAll(ballot int64, index int64, state comm.InstanceState,
 	cmdType comm.CommandType) *comm.Instance {
-	return &comm.Instance{Ballot: ballot, Command: &comm.Command{Type: cmdType},
-		Index: index, State: state, ClientId: 0}
+	instance := MakeInstance(ballot, index)
+	instance.State = state
+	instance.Command.Type = cmdType
+	return instance
 }
 
