8ba45e8f87e977e407a64f07fff0fc96907e50d5
Abutalib Aghayev <agayev@psu.edu>
2022-08-22 09:07:49 -0400

    c++: replicant: switch to async i/o, missing changes from prev commit

--- c++/replicant.cc
+++ c++/replicant.cc
@@ -12,75 +12,56 @@
 using asio::ip::tcp;
 using nlohmann::json;
 
-using multipaxos::Command;
-using multipaxos::CommandType::DEL;
-using multipaxos::CommandType::GET;
-using multipaxos::CommandType::PUT;
-
 Replicant::Replicant(json const& config)
-    : mp_(&log_, config),
-      kv_store_(std::make_unique<MemKVStore>()),
-      id_(config["id"]),
-      next_client_id_(id_),
+    : id_(config["id"]),
       num_peers_(config["peers"].size()),
-      acceptor_(io_) {
-  std::string me = config["peers"][id_];
-  auto pos = me.find(":") + 1;
+      mp_(&log_, config),
+      kv_store_(std::make_unique<MemKVStore>()),
+      ip_port_(config["peers"][id_]),
+      acceptor_(io_),
+      client_manager_(id_, num_peers_, &mp_) {}
+
+void Replicant::Start() {
+  mp_.Start();
+  StartExecutorThread();
+  StartServer();
+  io_.run();
+}
+
+void Replicant::Stop() {
+  StopExecutorThread();
+  StopServer();
+  mp_.Stop();
+}
+
+void Replicant::StartServer() {
+  auto pos = ip_port_.find(":") + 1;
   CHECK_NE(pos, std::string::npos);
-  std::string ip = me.substr(0, pos);
-  int port = std::stoi(me.substr(pos)) + 1;
+  int port = std::stoi(ip_port_.substr(pos)) + 1;
 
   tcp::endpoint endpoint(tcp::v4(), port);
   acceptor_.open(endpoint.protocol());
   acceptor_.set_option(tcp::acceptor::reuse_address(true));
   acceptor_.bind(endpoint);
   acceptor_.listen(5);
-  DLOG(INFO) << "replicant " << id_ << " accepting at " << ip << ":" << port;
+  DLOG(INFO) << id_ << " starting server at port " << port;
+
+  AcceptClient();
 }
 
-void Replicant::Start() {
-  mp_.Start();
-  StartExecutorThread();
-  for (;;) {
-    auto client_id = NextClientId();
-    auto socket = CreateSocket(client_id);
-    asio::error_code ec;
-    acceptor_.accept(*socket, ec);
-    if (ec)
-      break;
-    client_threads_.emplace_back(&Replicant::HandleClient, this, client_id,
-                                 socket);
-  }
-}
-
-void Replicant::Stop() {
-  for (auto& t : client_threads_)
-    t.join();
-  StopExecutorThread();
+void Replicant::StopServer() {
   acceptor_.close();
-  mp_.Stop();
+  client_manager_.StopAll();
 }
 
-void Replicant::HandleClient(int64_t client_id, tcp::socket* socket) {
-  for (;;) {
-    auto command = ReadCommand(socket);
-    if (command) {
-      auto r = mp_.Replicate(std::move(*command), client_id);
-      if (r.type_ == ResultType::kOk)
-        continue;
-      asio::error_code ec;
-      if (r.type_ == ResultType::kRetry) {
-        asio::write(*socket, asio::buffer("retry"), ec);
-      } else {
-        CHECK(r.type_ == ResultType::kSomeoneElseLeader);
-        asio::write(*socket, asio::buffer("the leader is..."), ec);
-      }
-    } else {
-      break;
-    }
-  }
-  socket->close();
-  RemoveSocket(client_id);
+void Replicant::AcceptClient() {
+  acceptor_.async_accept([this](std::error_code ec, tcp::socket socket) {
+    if (!acceptor_.is_open())
+      return;
+    CHECK(!ec);
+    client_manager_.Start(std::move(socket));
+    AcceptClient();
+  });
 }
 
 void Replicant::StartExecutorThread() {
@@ -100,44 +81,6 @@ void Replicant::ExecutorThread() {
     if (!r)
       break;
     auto [client_id, result] = *r;
-    auto socket = FindSocket(client_id);
-    if (!socket)
-      continue;
-    asio::error_code ec;
-    if (!result.ok_)
-      asio::write(*socket, asio::buffer("failed"), ec);
-    asio::write(*socket, asio::buffer(*result.value_), ec);
+    client_manager_.Respond(client_id, *result.value_);
   }
-}
-
-std::optional<Command> Replicant::ReadCommand(tcp::socket* cli) {
-  std::string line = ReadLine(cli);
-  if (line.empty())
-    return std::nullopt;
-
-  Command command;
-  if (strncmp(line.c_str(), "get", 3) == 0) {
-    command.set_type(GET);
-    command.set_key(line.substr(4));
-  } else if (strncmp(line.c_str(), "del", 3) == 0) {
-    command.set_type(DEL);
-    command.set_key(line.substr(4));
-  } else {
-    CHECK(strncmp(line.c_str(), "put", 3) == 0);
-    size_t p = line.find(":", 4);
-    command.set_type(PUT);
-    command.set_key(line.substr(4, p - 4));
-    command.set_value(line.substr(p + 1));
-  }
-  return command;
-}
-
-std::string Replicant::ReadLine(tcp::socket* cli) {
-  std::string line;
-  asio::streambuf request;
-  asio::error_code ec;
-  asio::read_until(*cli, request, '\n', ec);
-  if (ec.value() == 0)
-    std::getline(std::istream(&request), line);
-  return line;
 }
--- c++/replicant.h
+++ c++/replicant.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <optional>
 
+#include "client_manager.h"
 #include "json_fwd.h"
 #include "log.h"
 #include "memkvstore.h"
@@ -17,55 +18,25 @@ class Replicant {
   void Stop();
 
  private:
-  void HandleClient(int64_t client_id, asio::ip::tcp::socket* socket);
+  void StartServer();
+  void StopServer();
 
   void StartExecutorThread();
   void StopExecutorThread();
 
   void ExecutorThread();
 
-  std::optional<multipaxos::Command> ReadCommand(asio::ip::tcp::socket* cli);
-  std::string ReadLine(asio::ip::tcp::socket* cli);
+  void AcceptClient();
 
-  int64_t NextClientId() {
-    auto id = next_client_id_;
-    next_client_id_ += num_peers_;
-    return id;
-  }
-
-  asio::ip::tcp::socket* CreateSocket(int64_t client_id) {
-    std::scoped_lock lock(mu_);
-    auto [it, ok] =
-        client_sockets_.insert({client_id, asio::ip::tcp::socket(io_)});
-    CHECK(ok);
-    return &it->second;
-  }
-
-  asio::ip::tcp::socket* FindSocket(int64_t client_id) {
-    std::scoped_lock lock(mu_);
-    auto it = client_sockets_.find(client_id);
-    if (it == client_sockets_.end())
-      return nullptr;
-    return &it->second;
-  }
-
-  void RemoveSocket(int64_t client_id) {
-    std::scoped_lock lock(mu_);
-    auto ok = client_sockets_.erase(client_id);
-    CHECK(ok);
-  }
-
+  int64_t id_;
+  int64_t num_peers_;
   Log log_;
   MultiPaxos mp_;
   std::unique_ptr<KVStore> kv_store_;
-  int64_t id_;
-  int64_t next_client_id_;
-  int64_t num_peers_;
-  std::mutex mu_;
-  std::unordered_map<int64_t, asio::ip::tcp::socket> client_sockets_;
+  std::string ip_port_;
   asio::io_context io_;
   asio::ip::tcp::acceptor acceptor_;
-  std::vector<std::thread> client_threads_;
+  ClientManager client_manager_;
   std::thread executor_thread_;
 };
 
