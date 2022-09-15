5df03e7ced3acb8adff59b56d2021daaf26010dd
Abutalib Aghayev <agayev@psu.edu>
2022-09-15 12:43:30 -0400

    rust: multipaxos: introduce MultiPaxosInner and move all the ...
    ... shared state into it. This allows us to implement {start,stop}_xxx_thread
    functions.

--- rust/replicant/src/main.rs
+++ rust/replicant/src/main.rs
@@ -1,11 +1,32 @@
+use ::log::debug;
+
 mod kvstore;
 mod log;
 mod multipaxos;
 
-use kvstore::memkvstore::MemKVStore;
 use crate::log::Log;
+use kvstore::memkvstore::MemKVStore;
+use multipaxos::MultiPaxos;
+use std::{thread, time};
 
 fn main() {
-    let store = Box::new(MemKVStore::new());
-    let _log = Log::new(store);
+    env_logger::init();
+
+    let str = r#"{
+      "id": 0,
+      "threadpool_size": 8,
+      "commit_interval": 3000,
+      "peers": ["127.0.0.1:10000",
+                "127.0.0.1:11000",
+                "127.0.0.1:12000"]
+     }"#;
+
+    let config = serde_json::from_str(str).unwrap();
+
+    let log = Log::new(Box::new(MemKVStore::new()));
+    let mut mp = MultiPaxos::new(log, &config);
+    debug!("starting...");
+    mp.start_prepare_thread();
+    thread::sleep(time::Duration::from_millis(4000));
+    mp.stop_prepare_thread();
 }
--- rust/replicant/src/multipaxos/mod.rs
+++ rust/replicant/src/multipaxos/mod.rs
@@ -1,8 +1,7 @@
-use super::log::Log;
+use super::log::{Log, MapLog};
 use crate::kvstore::memkvstore::MemKVStore;
 use log::debug;
 use rand::distributions::{Distribution, Uniform};
-use rand::rngs::ThreadRng;
 use serde_json::json;
 use serde_json::Value as json;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -11,25 +10,29 @@ use std::{thread, time};
 
 tonic::include_proto!("multipaxos");
 
-struct MultiPaxos {
+pub struct MultiPaxos {
+    multi_paxos: Arc<MultiPaxosInner>,
+    prepare_thread: Option<thread::JoinHandle<()>>,
+    commit_thread: Option<thread::JoinHandle<()>>,
+}
+
+#[derive(Default)]
+struct AtomicState {
+    ballot: i64,
+    rpc_server_running: bool,
+}
+
+struct MultiPaxosInner {
+    atomic_state: Mutex<AtomicState>,
     ready: AtomicBool,
-    state: Mutex<State>,
-    log: Arc<Log>,
+    log: Log,
     id: i64,
     commit_received: AtomicBool,
     commit_interval: u64,
-    rng: ThreadRng,
-    dist: Uniform<u64>,
     prepare_thread_running: AtomicBool,
     commit_thread_running: AtomicBool,
     cv_leader: Condvar,
     cv_follower: Condvar,
-}
-
-#[derive(Default)]
-struct State {
-    ballot: i64,
-    rpc_server_running: bool,
 }
 
 const ID_BITS: i64 = 0xff;
@@ -49,17 +52,49 @@ fn is_someone_else_leader(ballot: i64, id: i64) -> bool {
 }
 
 impl MultiPaxos {
-    fn new(log: Arc<Log>, config: &json) -> Self {
+    pub fn new(log: Log, config: &json) -> Self {
+        Self {
+            multi_paxos: Arc::new(MultiPaxosInner::new(log, config)),
+            prepare_thread: None,
+            commit_thread: None,
+        }
+    }
+
+    pub fn start_prepare_thread(&mut self) {
+        debug!("{} starting prepare thread", self.multi_paxos.id);
+        self.multi_paxos
+            .prepare_thread_running
+            .store(true, Ordering::Relaxed);
+        self.prepare_thread = {
+            let multi_paxos = self.multi_paxos.clone();
+            Some(std::thread::spawn(move || multi_paxos.prepare_thread_fn()))
+        }
+    }
+
+    pub fn stop_prepare_thread(&mut self) {
+        debug!("{} stopping prepare thread", self.multi_paxos.id);
+        assert!(self
+            .multi_paxos
+            .prepare_thread_running
+            .load(Ordering::Relaxed));
+        self.multi_paxos
+            .prepare_thread_running
+            .store(false, Ordering::Relaxed);
+        self.multi_paxos.cv_follower.notify_one();
+        self.prepare_thread.take().unwrap().join().unwrap();
+    }
+}
+
+impl MultiPaxosInner {
+    fn new(log: Log, config: &json) -> Self {
         let ci = config["commit_interval"].as_u64().unwrap();
         Self {
+            atomic_state: Mutex::new(AtomicState::default()),
             ready: AtomicBool::new(false),
-            state: Mutex::new(State::default()),
             log: log,
             id: config["id"].as_i64().unwrap(),
             commit_received: AtomicBool::new(false),
             commit_interval: ci,
-            rng: rand::thread_rng(),
-            dist: Uniform::new(0, ci / 2),
             prepare_thread_running: AtomicBool::new(false),
             commit_thread_running: AtomicBool::new(false),
             cv_leader: Condvar::new(),
@@ -68,53 +103,57 @@ impl MultiPaxos {
     }
 
     fn next_ballot(&self) -> i64 {
-        let state = self.state.lock().unwrap();
-        let mut next_ballot = state.ballot;
+        let atomic_state = self.atomic_state.lock().unwrap();
+        let mut next_ballot = atomic_state.ballot;
         next_ballot += ROUND_INCREMENT;
         next_ballot = (next_ballot & !ID_BITS) | self.id;
         next_ballot
     }
 
-    fn become_leader(&mut self, next_ballot: i64) {
-        let mut state = self.state.lock().unwrap();
+    fn become_leader(&self, next_ballot: i64) {
+        let mut atomic_state = self.atomic_state.lock().unwrap();
         debug!(
             "{} became a leader: ballot: {} -> {}",
-            self.id, state.ballot, next_ballot
+            self.id, atomic_state.ballot, next_ballot
         );
-        state.ballot = next_ballot;
-        *self.ready.get_mut() = false;
+        atomic_state.ballot = next_ballot;
+        self.ready.store(false, Ordering::Relaxed);
         self.cv_leader.notify_one();
     }
 
-    fn become_follower(&self, state: &mut State, next_ballot: i64) {
-        let prev_leader = leader(state.ballot);
+    fn become_follower(&self, atomic_state: &mut AtomicState, next_ballot: i64) {
+        let prev_leader = leader(atomic_state.ballot);
         let next_leader = leader(next_ballot);
         if next_leader != self.id && (prev_leader == self.id || prev_leader == MAX_NUM_PEERS) {
             debug!(
                 "{} became a follower: ballot: {} -> {}",
-                self.id, state.ballot, next_ballot
+                self.id, atomic_state.ballot, next_ballot
             );
             self.cv_follower.notify_one();
         }
-        state.ballot = next_ballot;
+        atomic_state.ballot = next_ballot;
     }
 
-    fn ballot(&mut self) -> (i64, bool) {
-        let state = self.state.lock().unwrap();
-        (state.ballot, *self.ready.get_mut())
+    fn ballot(&self) -> (i64, bool) {
+        let atomic_state = self.atomic_state.lock().unwrap();
+        (atomic_state.ballot, self.ready.load(Ordering::Relaxed))
     }
 
-    fn wait_until_leader(&mut self) {
-        let mut state = self.state.lock().unwrap();
-        while *self.commit_thread_running.get_mut() && !is_leader(state.ballot, self.id) {
-            state = self.cv_leader.wait(state).unwrap();
+    fn wait_until_leader(&self) {
+        let mut atomic_state = self.atomic_state.lock().unwrap();
+        while self.commit_thread_running.load(Ordering::Relaxed)
+            && !is_leader(atomic_state.ballot, self.id)
+        {
+            atomic_state = self.cv_leader.wait(atomic_state).unwrap();
         }
     }
 
-    fn wait_until_follower(&mut self) {
-        let mut state = self.state.lock().unwrap();
-        while *self.prepare_thread_running.get_mut() && is_leader(state.ballot, self.id) {
-            state = self.cv_follower.wait(state).unwrap();
+    fn wait_until_follower(&self) {
+        let mut atomic_state = self.atomic_state.lock().unwrap();
+        while self.prepare_thread_running.load(Ordering::Relaxed)
+            && is_leader(atomic_state.ballot, self.id)
+        {
+            atomic_state = self.cv_follower.wait(atomic_state).unwrap();
         }
     }
 
@@ -122,15 +161,44 @@ impl MultiPaxos {
         thread::sleep(time::Duration::from_millis(self.commit_interval));
     }
 
-    fn sleep_for_random_interval(&mut self) {
-        let sleep_time =
-            self.commit_interval + self.commit_interval / 2 + self.dist.sample(&mut self.rng);
+    fn sleep_for_random_interval(&self) {
+        let ci = self.commit_interval as u64;
+        let dist = Uniform::new(0, (ci / 2) as u64);
+        let sleep_time = ci + ci / 2 + dist.sample(&mut rand::thread_rng());
         thread::sleep(time::Duration::from_millis(sleep_time));
     }
 
     fn received_commit(&self) -> bool {
         self.commit_received.swap(false, Ordering::Relaxed)
     }
+
+    fn start_prepare_thread(&self) {
+        debug!("{} starting prepare thread", self.id);
+        self.prepare_thread_running.store(true, Ordering::Relaxed);
+    }
+
+    fn prepare_thread_fn(&self) {
+        while self.prepare_thread_running.load(Ordering::Relaxed) {
+            self.wait_until_follower();
+            while self.prepare_thread_running.load(Ordering::Relaxed) {
+                self.sleep_for_random_interval();
+                if self.received_commit() {
+                    continue;
+                }
+                let next_ballot = self.next_ballot();
+                if let Some(log) = self.run_prepare_phase(next_ballot) {
+                    self.become_leader(next_ballot);
+                    self.replay(next_ballot, &log);
+                }
+            }
+        }
+    }
+
+    fn run_prepare_phase(&self, ballot: i64) -> Option<MapLog> {
+        None
+    }
+
+    fn replay(&self, ballot: i64, log: &MapLog) {}
 }
 
 #[cfg(test)]
@@ -155,7 +223,6 @@ mod tests {
     #[derive(Default)]
     struct TestState {
         configs: Vec<json>,
-        logs: Vec<Arc<Log>>,
         peers: Vec<MultiPaxos>,
     }
 
@@ -164,11 +231,10 @@ mod tests {
         let mut state = TestState::default();
         for id in 0..NUM_PEERS {
             let config = make_config(id, NUM_PEERS);
-            let log = Arc::new(Log::new(Box::new(MemKVStore::new())));
-            let peer = MultiPaxos::new(log.clone(), &config);
+            let log = Log::new(Box::new(MemKVStore::new()));
+            let peer = MultiPaxos::new(log, &config);
 
             state.configs.push(config);
-            state.logs.push(log);
             state.peers.push(peer);
         }
         state
@@ -180,7 +246,7 @@ mod tests {
 
         for id in 0..NUM_PEERS {
             let ballot = id + ROUND_INCREMENT;
-            assert_eq!(ballot, state.peers[id as usize].next_ballot());
+            assert_eq!(ballot, state.peers[id as usize].multi_paxos.next_ballot());
         }
     }
 }
