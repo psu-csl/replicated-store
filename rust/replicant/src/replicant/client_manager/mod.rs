f70ad5e2e7ee7d40628e0ffe503175fe814e31ea
Abutalib Aghayev <agayev@psu.edu>
2022-09-29 16:46:54 -0400

    rust: client_manager: introduced client manager struct

---
+++ rust/replicant/src/replicant/client_manager/mod.rs
@@ -0,0 +1,117 @@
+use crate::replicant::multipaxos::MultiPaxos;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::str;
+use std::sync::{Arc, Mutex};
+use tokio::io::AsyncReadExt;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::{TcpListener, TcpStream};
+
+struct Client {
+    id: i64,
+    socket: OwnedReadHalf,
+    client_manager: Arc<ClientManagerInner>,
+    multi_paxos: Arc<MultiPaxos>,
+}
+
+impl Client {
+    fn new(
+        id: i64,
+        socket: OwnedReadHalf,
+        client_manager: Arc<ClientManagerInner>,
+        multi_paxos: Arc<MultiPaxos>,
+    ) -> Self {
+        Self {
+            id,
+            socket,
+            client_manager,
+            multi_paxos,
+        }
+    }
+
+    async fn start(&mut self) {
+        let mut buf = vec![0; 1024];
+        loop {
+            let num_read = self.socket.read(&mut buf).await.unwrap();
+            if num_read == 0 {
+                break;
+            }
+            println!("read {}", str::from_utf8(&buf).unwrap());
+        }
+        self.client_manager.stop(self.id);
+    }
+}
+
+struct ClientManagerInner {
+    next_id: Mutex<i64>,
+    num_peers: i64,
+    clients: Mutex<HashMap<i64, OwnedWriteHalf>>,
+}
+
+impl ClientManagerInner {
+    fn new(id: i64, num_peers: i64) -> Self {
+        Self {
+            next_id: Mutex::new(id),
+            num_peers,
+            clients: Mutex::new(HashMap::new()),
+        }
+    }
+
+    fn next_client_id(&self) -> i64 {
+        let mut next_id = self.next_id.lock().unwrap();
+        let id = *next_id;
+        *next_id += self.num_peers;
+        id
+    }
+
+    fn start(&self, write_half: OwnedWriteHalf) -> i64 {
+        let id = self.next_client_id();
+        let mut clients = self.clients.lock().unwrap();
+        let v = clients.insert(id, write_half);
+        drop(clients);
+        assert!(v.is_none());
+        println!("client_manager started client {}", id);
+        id
+    }
+
+    fn stop(&self, id: i64) {
+        let mut clients = self.clients.lock().unwrap();
+        let v = clients.remove(&id);
+        drop(clients);
+        assert!(v.is_some());
+        println!("client_manager stopped client {}", id);
+    }
+
+    fn stop_all(&self) {
+        let mut clients = self.clients.lock().unwrap();
+        clients.clear();
+    }
+}
+
+pub struct ClientManager {
+    client_manager: Arc<ClientManagerInner>,
+}
+
+impl ClientManager {
+    pub fn new(id: i64, num_peers: i64) -> Self {
+        Self {
+            client_manager: Arc::new(ClientManagerInner::new(id, num_peers)),
+        }
+    }
+
+    pub fn start(&self, client: TcpStream, multi_paxos: Arc<MultiPaxos>) {
+        let (read_half, write_half) = client.into_split();
+        let id = self.client_manager.start(write_half);
+        let client_manager = self.client_manager.clone();
+        tokio::spawn(async move {
+            Client::new(id, read_half, client_manager, multi_paxos)
+                .start()
+                .await;
+        });
+    }
+
+    fn stop_all(&self) {
+        self.client_manager.stop_all();
+    }
+}
--- rust/replicant/src/replicant/mod.rs
+++ rust/replicant/src/replicant/mod.rs
@@ -1,37 +1,42 @@
-pub mod kvstore;
-pub mod log;
-pub mod multipaxos;
+mod client_manager;
+mod kvstore;
+mod log;
+mod multipaxos;
 
+use crate::replicant::client_manager::ClientManager;
 use crate::replicant::kvstore::memkvstore::MemKVStore;
 use crate::replicant::log::Log;
 use crate::replicant::multipaxos::MultiPaxos;
 use ::log::info;
 use serde_json::Value as json;
+use std::net::SocketAddr;
 use std::sync::Arc;
+use tokio::net::TcpListener;
 use tokio::sync::oneshot::Sender;
 
 struct ReplicantInner {
     id: i64,
-    num_peers: i64,
     log: Arc<Log>,
     ip_port: String,
-    multi_paxos: MultiPaxos,
+    multi_paxos: Arc<MultiPaxos>,
+    client_manager: ClientManager,
 }
 
 impl ReplicantInner {
     fn new(config: &json) -> Self {
         let id = config["id"].as_i64().unwrap();
         let peers = config["peers"].as_array().unwrap();
-        let num_peers = peers.len() as i64;
         let ip_port = peers[id as usize].as_str().unwrap().to_string();
         let log = Arc::new(Log::new(Box::new(MemKVStore::new())));
-        let multi_paxos_log = log.clone();
+        let multi_paxos = Arc::new(MultiPaxos::new(log.clone(), config));
+        let client_manager = ClientManager::new(id, peers.len() as i64);
+
         Self {
             id,
-            num_peers,
             log,
             ip_port,
-            multi_paxos: MultiPaxos::new(multi_paxos_log, config),
+            multi_paxos,
+            client_manager,
         }
     }
 
@@ -42,6 +47,21 @@ impl ReplicantInner {
                 break;
             }
             let (id, result) = r.unwrap();
+        }
+    }
+
+    async fn server_task_fn(&self) {
+        let mut addr: SocketAddr = self.ip_port.parse().unwrap();
+        addr.set_port(addr.port() + 1);
+        let listener = TcpListener::bind(addr).await.unwrap();
+
+        loop {
+            match listener.accept().await {
+                Ok((client, _)) => {
+                    self.client_manager.start(client, self.multi_paxos.clone())
+                }
+                Err(_) => break,
+            }
         }
     }
 }
@@ -60,20 +80,38 @@ impl Replicant {
     pub fn start(&self) -> Sender<()> {
         let tx = self.replicant.multi_paxos.start();
         self.start_executor_task();
+        self.start_server_task();
         tx
     }
 
     pub fn stop(&self, tx: Sender<()>) {
+        self.stop_server_task();
         self.stop_executor_task();
         self.replicant.multi_paxos.stop(tx);
     }
 
     pub fn start_executor_task(&self) {
         info!("{} starting executor task", self.replicant.id);
+        let replicant = self.replicant.clone();
+        tokio::spawn(async move {
+            replicant.executor_task_fn().await;
+        });
     }
 
     pub fn stop_executor_task(&self) {
         info!("{} stopping executor task", self.replicant.id);
         self.replicant.log.stop();
+    }
+
+    pub fn start_server_task(&self) {
+        info!("{} starting server task", self.replicant.id);
+        let replicant = self.replicant.clone();
+        tokio::spawn(async move {
+            replicant.server_task_fn().await;
+        });
+    }
+
+    pub fn stop_server_task(&self) {
+        info!("{} stopping server task", self.replicant.id);
     }
 }
