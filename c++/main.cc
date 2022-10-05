57f0d3aeb53567ba34813bd512b988feb9f335c1
Abutalib Aghayev <agayev@psu.edu>
2022-10-05 15:44:28 -0400

    c++: main: misc variable rename for clarity

--- c++/main.cc
+++ c++/main.cc
@@ -10,14 +10,14 @@
 using nlohmann::json;
 
 DEFINE_uint32(id, 0, "my id in the peers array in configuration file");
-DEFINE_string(config, "config.json", "path to the configuration file");
+DEFINE_string(config_path, "config.json", "path to the configuration file");
 
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   FLAGS_logtostderr = 1;
 
-  std::ifstream f(FLAGS_config);
+  std::ifstream f(FLAGS_config_path);
   CHECK(f);
 
   json config;
