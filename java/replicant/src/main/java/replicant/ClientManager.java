0a6df9ab5160a85cf4eeb1e54adfee79fe7414d8
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2022-11-03 23:10:56 +0300

    java: client_manager: add lock and replace concurrent with normal map

--- java/replicant/src/main/java/replicant/ClientManager.java
+++ java/replicant/src/main/java/replicant/ClientManager.java
@@ -5,21 +5,23 @@ import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.AttributeKey;
-
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.slf4j.LoggerFactory;
 import multipaxos.MultiPaxos;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Sharable
 public class ClientManager extends SimpleChannelInboundHandler<String> {
 
   private static final Logger logger = (Logger) LoggerFactory.getLogger(ClientManager.class);
-  private final ConcurrentHashMap<Long, Client> clients = new ConcurrentHashMap<>();
+  private long nextClientId;
   private final long numPeers;
   private final MultiPaxos multiPaxos;
-  private long nextClientId;
   private final AttributeKey<Long> clientIdAttrKey = AttributeKey.valueOf("ClientID");
+
+  private final ReentrantLock mu = new ReentrantLock();
+  private final HashMap<Long, Client> clients = new HashMap<>();
 
   public ClientManager(long id, long numPeers, MultiPaxos multiPaxos) {
     this.nextClientId = id;
@@ -37,28 +39,36 @@ public class ClientManager extends SimpleChannelInboundHandler<String> {
     var id = nextClientId();
     logger.info("client joined " + ctx);
     ctx.channel().attr(clientIdAttrKey).set(id);
+    mu.lock();
     clients.put(id, new Client(id,ctx.channel(),multiPaxos));
+    mu.unlock();
   }
 
   @Override
   public void channelUnregistered(ChannelHandlerContext ctx) {
     var id = ctx.channel().attr(clientIdAttrKey).get();
+    mu.lock();
     var it = clients.get(id);
     assert it!=null;
     clients.remove(id);
+    mu.unlock();
   }
 
   @Override
   public void channelRead0(ChannelHandlerContext ctx, String msg) {
     var clientId = ctx.channel().attr(clientIdAttrKey).get();
+    mu.lock();
     var client = clients.get(clientId);
+    mu.unlock();
     if(client!=null)
       client.read(msg);
-
   }
-
-
   public Client get(Long clientId){
-    return clients.get(clientId);
+    mu.lock();
+    try {
+      return clients.get(clientId);
+    }finally {
+      mu.unlock();
+    }
   }
 }
